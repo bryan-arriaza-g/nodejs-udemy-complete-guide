5c2bd2513e5fff31830ba0ef762848beced5a006
barriaza <bjarriaza.g.07@gmail.com>
2020-10-16 00:23:38 -0600

    Agregando reglas eslint, airbnb, prettier, husky, lintstaged

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,12 +5,12 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -5,7 +5,8 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log('prueba');
 });
 
 module.exports = router;
