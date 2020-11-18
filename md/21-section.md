e9ddf0b4a9749051784acc01c4c2ec42d4eac211
barriaza <bryan.arriaza.g@gmail.com>
2020-11-18 00:39:16 -0600

    339. Skip & Limit with SQL 340. Preparing Pagination Data on the Server

--- md/21-section.md
+++ md/21-section.md
@@ -15,3 +15,5 @@ Master Node JS & Deno.js, build REST APIs with Node.js, GraphQL APIs, add Authen
 336. Module Introduction
 337. Adding Pagination Links
 338. Retrieving a Chunk of Data
+339. Skip & Limit with SQL
+340. Preparing Pagination Data on the Server
--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -41,14 +41,26 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   const { page } = req.query;
+  let totalItems;
   Product.find()
-    .skip((page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
